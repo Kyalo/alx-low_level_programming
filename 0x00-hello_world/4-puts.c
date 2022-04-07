536336352bec04f12c342d217e610776f2652737
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-07 14:58:36 -0700

    Added Betty style documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,16 @@
+/*
+ * File: 4-puts.c
+ * Auth: Maurice Kyalo
+ */
+
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 
 int main(void)
 {
@@ -7,4 +18,3 @@ int main(void)
 
 	return (0);
 }
-
