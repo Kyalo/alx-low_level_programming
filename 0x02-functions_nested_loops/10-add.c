8244eae5f7a40fd71a67a024fb37cab9f4fd7112
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-12 16:50:29 -0700

    Completed task 10 and updated main.h file

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -7,8 +7,9 @@
 
 /**
  * add - adds two integers and returns the result.
- *@a: The first number to be added.
- *@b: The second number to be added.
+ * @a: The first number to be added.
+ * @b: The second number to be added.
+ *
  * Return- Always(0).
  */
 int add(int a, int b)
