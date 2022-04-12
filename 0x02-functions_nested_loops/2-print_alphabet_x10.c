a70d913de7ea8743a4989971b21dacecb4c42b42
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-12 14:33:36 -0700

    Completed task 2 and updated main.h file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Maurice Kyalo
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase followed by a new line.
+ */
+void print_alphabet_X10(void)
+{
+	char letter, counter;
+
+	for (counter = 0; counter < 10; counter++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		
+		_putchar('\n);
+	}
+}
