7a311b59c8c0d9410135d04b6ceb3c9ecacb0d46
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-12 15:44:33 -0700

    Updated Betty coding style

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,7 +9,9 @@
  * print_sign - prints the sign of a number.
  * @n: The number being checked.
  *
- * Return: 1 and print '+' if n is greater than zero, 0 and print '0' if n is zero, -1 and print '-' if n is less than zero.
+ * Return: 1 and print '+' if n is greater than zero,
+ * 	0 and print '0' if n is zero,
+ * 	-1 and print '-' if n is less than zero.
  */
 int print_sign(int n)
 {
@@ -18,7 +20,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if(n == 0)
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
