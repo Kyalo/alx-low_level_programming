e60372468b54a890cc6b84ddc9aee6e50d8a8516
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-12 16:48:25 -0700

    Completed task 9 and updated main.h file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+/*
+ * File: 10-add.c
+ * Auth: Maurice Kyalo
+ */
+
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ *@a: The first number to be added.
+ *@b: The second number to be added.
+ * Return- Always(0).
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
