20cda4b8b2e071ee3e3042ae167b66b7cf331381
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-14 15:34:13 -0700

    Made corrections

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,12 +3,12 @@
  * Auth: Maurice Kyalo
  */
 
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
