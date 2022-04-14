06f179109674e640733249326cccaaaa6175eb91
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-14 12:33:01 -0700

    Completed task 0 and updated main.h and readme file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-main.c
+ * Auth: Maurice Kyalo
+ */
+
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
