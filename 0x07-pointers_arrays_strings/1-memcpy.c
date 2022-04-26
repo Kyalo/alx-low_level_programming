48d2598ee6425c24d317ae151b53dde581cabc97
Maurice Kyalo <mrckyalo@gmail.com>
2022-04-26 16:15:12 -0700

    Made corrections

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -17,16 +17,10 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    char *destination;
+	unsigned int i;
 
-    yolo = dest;
-    while (n > 0)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
 
-    return (destination);
+	return (dest);
 }
