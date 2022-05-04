2f171f919a6c5770b59e35694db1112c03de15a4
Maurice Kyalo <mrckyalo@gmail.com>
2022-05-03 19:15:04 -0700

    Completed task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Maurice Kyalo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the program name followed by a new line
+ * @argc: the number of arguments to the program
+ * @argv: an arrya of pointers to the arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
